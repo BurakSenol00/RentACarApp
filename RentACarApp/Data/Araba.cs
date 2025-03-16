504a57fd3aba7a58d4330df745255345d0df446a
BurakSenol00 <buraksenol00@outlook.com>
2025-03-16 17:21:07 +0300

    Creates interface and necessary classes

---
+++ RentACarApp/Data/Araba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Data
+{
+    public class Araba : IArac
+    {
+        public string Marka { get; set; }
+        public string Model { get ; set ; }
+        public int Yil { get; set; }
+        public string Renk { get ; set; }
+        public decimal GunlukUcret { get; set; }
+        public bool MusaitlikDurumu { get; set; }
+        public AracTipi AracTipi { get; set; }
+        public decimal ToplamUcret { get; set; }
+
+        public void BilgiGoster()
+        {
+           
+        }
+    }
+}
